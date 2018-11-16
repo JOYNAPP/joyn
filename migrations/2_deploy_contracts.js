8c7043a86bb6c68383aabbcb377899ba30e83353
Uma Huggins <umahuggins@gmail.com>
2018-11-16 15:45:58 -0500

    contracts added

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,14 @@
+var SimpleStorage = artifacts.require("SimpleStorage");
+var TutorialToken = artifacts.require("TutorialToken");
+var ComplexStorage = artifacts.require("ComplexStorage");
+var Adoption = artifacts.require("Adoption");
+var JoinUp = artifacts.require("JoinUp");
+
+
+module.exports = function(deployer) {
+  deployer.deploy(SimpleStorage);
+  deployer.deploy(TutorialToken);
+  deployer.deploy(ComplexStorage);
+  deployer.deploy(Adoption);
+  deployer.deploy(JoinUp);
+};
